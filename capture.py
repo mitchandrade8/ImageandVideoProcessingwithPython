dafe86ddd3f13d9c08b9660f63f8a90ca1cd78d2
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 10:07:05 -0600

    commented out some lines of code to see if i can get a better frames per second ratio. Will now test

--- capture.py
+++ capture.py
@@ -8,11 +8,11 @@ while True:
     a = a + 1
     check, frame = video.read()
 
-    print(check)
-    print(frame)
+    #print(check)
+    #print(frame)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    time.sleep(3)
+    #time.sleep(3)
     cv2.imshow("Capturing", frame)
 
     key = cv2.waitKey(1)
