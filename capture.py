7e9283390b3ecf1e00d74aeed5eea19849be13ab
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 10:28:15 -0600

    removing code from understanding certain things and now adding code for object detection

--- capture.py
+++ capture.py
@@ -2,17 +2,13 @@ import cv2, time
 
 video = cv2.VideoCapture(0)
 
-a = 0
 
 while True:
-    a = a + 1
+    
     check, frame = video.read()
 
-    #print(check)
-    #print(frame)
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #time.sleep(3)
+    
     cv2.imshow("Capturing", frame)
 
     key = cv2.waitKey(1)
@@ -20,7 +16,7 @@ while True:
     if key == ord('q'):
         break
 
-print(a)
+
 video.release()
 
 cv2.destroyAllWindows
