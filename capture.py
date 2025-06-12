4a282f96e4c43a2afe7f0a60e6512e55aa53c376
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 09:53:16 -0600

    improving the video capture

--- capture.py
+++ capture.py
@@ -2,6 +2,17 @@ import cv2, time
 
 video = cv2.VideoCapture(0)
 
-time.sleep(4)
+while True:
+    check, frame = video.read()
 
+    print(check)
+    print(frame)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    time.sleep(3)
+    cv2.imshow("Capturing", frame)
+
+    cv2.waitKey(0)
 video.release()
+
+cv2.destroyAllWindows
