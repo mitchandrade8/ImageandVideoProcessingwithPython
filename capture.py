fd8b62ceca1267aa5788bfc2d1b807277ad65be9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 10:30:23 -0600

    added a conditional for first_frame

--- capture.py
+++ capture.py
@@ -1,15 +1,18 @@
 import cv2, time
 
+first_frame = None
+
 video = cv2.VideoCapture(0)
 
-
 while True:
-    
     check, frame = video.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    cv2.imshow("Capturing", frame)
+    if first_frame is None:
+        first_frame = gray
+
+    cv2.imshow("Capturing", gray)
 
     key = cv2.waitKey(1)
     
