2657dab922e48b1955f11e1d6ddc55811440a886
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 08:31:41 -0600

    we now have a rectangle around the face in our current image.

--- face_detector.py
+++ face_detector.py
@@ -10,9 +10,12 @@ faces = face_cascade.detectMultiScale(gray_img,
                                       minNeighbors = 5
                                       ) 
 
+for x, y, w, h in faces:
+    img = cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255,0), 3)
+
 print(type(faces))
 print(faces)
 
-cv2.imshow("Gray", gray_img)
+cv2.imshow("Gray", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
