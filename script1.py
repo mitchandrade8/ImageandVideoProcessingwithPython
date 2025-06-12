d2434a514818757ac7580be106f1fb254a2f3086
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 06:48:28 -0600

    converted the new method to ints and got a nice mini version of the image

--- script1.py
+++ script1.py
@@ -7,7 +7,7 @@ print(img)
 print(img.shape)
 print(img.ndim)
 
-resized_image = cv2.resize(img,(1500, 1500))
+resized_image = cv2.resize(img,(int(img.shape[1]/2), int(img.shape[0]/2)))
 cv2.imshow("Galaxy", resized_image)
 cv2.waitKey(2000)
 cv2.destroyAllWindows()
