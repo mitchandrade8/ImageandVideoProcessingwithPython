c21e3d010b1dbf852f3d7de76e133993920811b1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 06:36:54 -0600

    resizing the image

--- script1.py
+++ script1.py
@@ -7,6 +7,7 @@ print(img)
 print(img.shape)
 print(img.ndim)
 
-cv2.imshow("Galaxy", img)
+resized_image = cv2.resize(img,(1500, 1500))
+cv2.imshow("Galaxy", resized_image)
 cv2.waitKey(2000)
 cv2.destroyAllWindows()
