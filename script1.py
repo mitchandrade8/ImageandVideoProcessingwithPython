bae6e0954d615adcb4854a3fe3758d28ec44ff31
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 06:56:30 -0600

    created a copy and named it from our image. added onw line of code for that functionalty

--- script1.py
+++ script1.py
@@ -9,5 +9,6 @@ print(img.ndim)
 
 resized_image = cv2.resize(img,(int(img.shape[1]/2), int(img.shape[0]/2)))
 cv2.imshow("Galaxy", resized_image)
+cv2.imwrite("Galaxy_resized.jpg", resized_image)
 cv2.waitKey(2000)
 cv2.destroyAllWindows()
