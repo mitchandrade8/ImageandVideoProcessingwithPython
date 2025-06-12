219fcfb57da60f3cf8417848c6943ceaee9c101d
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-12 07:11:03 -0600

    ran and compiled. Saw all the new images resized fast

--- script1.py
+++ script1.py
@@ -1,15 +1,17 @@
 import cv2
 import glob
 
-img = cv2.imread("galaxy.jpg", 1)
-
+images = glob.glob("*.jpg")
+''' 
 print(type(img))
 print(img)
 print(img.shape)
-print(img.ndim)
+print(img.ndim) '''
 
-resized_image = cv2.resize(img,(int(img.shape[1]/2), int(img.shape[0]/2)))
-cv2.imshow("Galaxy", resized_image)
-cv2.imwrite("Galaxy_resized.jpg", resized_image)
-cv2.waitKey(2000)
-cv2.destroyAllWindows()
+for image in images:
+    img = cv2.imread(image, 1)
+    re = cv2.resize(img,(1000, 1000))
+    cv2.imshow("2", re)
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
+    cv2.imwrite("resized" + image,re)
