5c5e668d78de4ba1c9ae3d2f1614bf5914300805
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-13 21:50:38 -0600

    cleaning up bad syntax

--- motion_detection.py
+++ motion_detection.py
@@ -18,14 +18,14 @@ while True:
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
 
-    (cnts, _) = cv2.findCountours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    for countour in cnts:
-        if cv2.contourArea(countour) < 1000:
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
             continue
 
-        (x, y, w, h) = cv2.boundingRect(countour)
-        cv2.rectangle(frame, (x, y) (x + w, y + h), (0, 255, 0), 3)
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
     cv2.imshow("Gray Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
