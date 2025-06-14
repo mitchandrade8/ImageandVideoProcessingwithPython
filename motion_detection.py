0ef9e6a51ba9b537d519919d22400043dc40e17a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-13 21:49:10 -0600

    error with the contour

--- motion_detection.py
+++ motion_detection.py
@@ -20,11 +20,17 @@ while True:
 
     (cnts, _) = cv2.findCountours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    
+    for countour in cnts:
+        if cv2.contourArea(countour) < 1000:
+            continue
+
+        (x, y, w, h) = cv2.boundingRect(countour)
+        cv2.rectangle(frame, (x, y) (x + w, y + h), (0, 255, 0), 3)
 
     cv2.imshow("Gray Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
     key = cv2.waitKey(1)
     print(gray)
