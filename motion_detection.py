386e63d13d90db8d4f562cc32e0db777deee5389
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-13 21:17:11 -0600

    added more code for our frames

--- motion_detection.py
+++ motion_detection.py
@@ -15,12 +15,16 @@ while True:
         continue
     
     delta_frame = cv2.absdiff(first_frame, gray)
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
 
-    thresh_delta = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    (cnts, _) = cv2.findCountours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    
 
     cv2.imshow("Gray Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
-    cv2.imshow("Threshold Frame", thresh_delta)
+    cv2.imshow("Threshold Frame", thresh_frame)
 
     key = cv2.waitKey(1)
     print(gray)
